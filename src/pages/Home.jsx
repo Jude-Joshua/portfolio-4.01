0f035f6cc95fe78e1f210ea9cd378dede9049142
Ping1234-tech <smithcomfort251@gmail.com>
2026-02-14 12:25:12 +0100

    cta section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -154,103 +154,89 @@ const Home = () => {
             </section>
 
             <section className="archetype flex flex-col items-start justify-start">
-                <article className="archetype-title flex flex-col items-start justify-start">
-                    <p className="t-highlights">
-                        My brand archetype would be...
-                    </p>
-                    <h2 className="h2">
-                        Sometimes, a <span className="archetype-title-highlights">Rebel</span>, often, an <span className="archetype-title-highlights">Explorer</span>, but always a <span className="archetype-title-highlights">Caregiver</span>.
-                    </h2>
-                </article>
+				<article className="archetype-title flex flex-col items-start justify-start">
+					<p className="t-highlights">
+						My brand archetype would be…
+					</p>
+					<h2 className="h2">
+						Sometimes, a <span className="archetype-title-highlights">Rebel</span>, often, an <span className="archetype-title-highlights">Explorer</span>, but always a <span className="archetype-title-highlights">Caregiver</span>.
+					</h2>
+				</article>
 
-                {/*  grid */}
-        <article className="archetype-grid grid grid-cols-1 md:grid-cols-2 gap-10 w-full">
-            <div className="archetype-body flex flex-col items-start justify-start w-full">
-                <div className="archetype-body-info grid grid-cols-7 justify-start items-start">
-                    <div className="archetype-body-info-left col-span-4 flex flex-col items-start justify-start">
-                        <div className="archetype-body-info-left-paragraphs flex flex-col items-start justify-start">
-                            <p className="t-copy">
-                                I enjoy finding new ways to make stuff work. This has always been my MO for my career, school and life in general.
-                            </p>
-                            <p className="t-copy">
-                                Whether it's figuring out how to design digital products and brands that meets user needs, playing with photography, or figuring out how to fix a broken gadget at home; I like to explore different approaches until I find one that works.
-                            </p>
-                            <p className="t-copy">
-                                I am interested in figuring out how things work, why people use them, and how small decisions shape everyday experiences.
-                            </p>
-                        </div>
-                        <div className="archetype-body-info-left-label flex flex-col items-start justify-start">
-                            <p className="t-highlights">I LOVE</p>
-                            <div className="archetype-body-info-left-label-list flex flex-row flex-wrap items-start justify-start">
-                                {[
-                                    "Books",
-                                    "Photography",
-                                    "Poetry",
-                                    "Art",
-                                    "Music",
-                                    "Nature",
-                                    "Mentoring",
-                                    "Black & White",
-                                ].map((item) => (
-                                    <span
-                                        key={item}
-                                        className="archetype-body-info-left-label-list-item t-copy-small"
-                                    >
-                                        {item}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<article className="archetype-body flex flex-col items-start justify-start w-full">
+					<div className="archetype-body-info grid grid-cols-7 justify-start items-start">
+						<div className="archetype-body-info-left col-span-4 flex flex-col items-start justify-start">
+							<div className="archetype-body-info-left-paragraphs flex flex-col items-start justify-start">
+								<p className="t-copy">
+									I enjoy finding new ways to make stuff work. This has always been my MO for my career, school and life in general.
+								</p>
+								<p className="t-copy">
+									Whether it’s figuring out how to design digital products and brands that meets user needs, playing with photography, or figuring out how to fix a broken gadget at home; I like to explore different approaches until I find one that works.
+								</p>
+								<p className="t-copy">
+									I’m interested in figuring how things work, why people use them, and how small decisions shape everyday experiences.
+								</p>
+							</div>
+							<div className="archetype-body-info-left-label flex flex-col items-start justify-start">
+								<p className="t-highlights">I LOVE…</p>
+								<div className="archetype-body-info-left-label-list flex flex-row flex-wrap items-start justify-start">
+									{[
+										"Books",
+										"Photography",
+										"Poetry",
+										"Art",
+										"Music",
+										"Nature",
+										"Mentoring",
+										"Black & White",
+									].map((item) => (
+										<span
+											key={item}
+											className="archetype-body-info-left-label-list-item t-copy-small"
+										>
+											{item}
+										</span>
+									))}
+								</div>
+							</div>
+						</div>
+						<div className="archetype-body-info-right col-span-3 flex flex-col items-start justify-start">
+							<div className='archetype-body-info-right-img'>
+								<ImageComponent src={CatImage} className={''} alt={''} />
+							</div>
 
-            <div className="archetype-body-right flex flex-col items-start justify-start w-full">
-                {/* Right image card */}
-                <div className=" archetype-right-card">
-                    <div className='archetype-right-card-img'>
-                        <ImageComponent src={CatImage} className={'archetype-img'} alt={''}/>
+							<div className="archetype-body-info-right-navigation flex items-center justify-between ">
+								<span className="archetype-body-info-right-navigation-icon active"><PawPrintIcon weight={'regular'} size={32} /></span>
+								<span className="archetype-body-info-right-navigation-icon"><CameraIcon weight={'regular'} size={32} /></span>
+								<span className="archetype-body-info-right-navigation-icon"><SneakerMoveIcon weight={'regular'} size={32} /></span>
+								<span className="archetype-body-info-right-navigation-icon"><RainbowCloudIcon weight={'regular'} size={32} /></span>
+								<span className="archetype-body-info-right-navigation-icon"><MicrophoneStageIcon weight={'regular'}size={32} /></span>
+							</div>
+						</div>
+					</div>
+					<div className="archetype-body-footer flex flex-row items-start justify-center w-full">
+						<div className="archetype-body-footer-card flex flex-col items-center">
+							<p className="t-highlights">Currently watching…</p>
+							<Button hierarchy={'tertiary'} type={'link'} link={''}>
+								<FilmSlateIcon weight={'bold'} fontSize={'24'} />
+								<span>Stranger Things S05</span>
+								<ArrowUpRightIcon weight={'bold'} fontSize={'24'} />
+							</Button>
+						</div>
+						<div className="divider vertical"/>
+						<div className="archetype-body-footer-card flex flex-col items-center">
+							<p className="t-highlights">Currently reading…</p>
+							<Button hierarchy={'tertiary'} type={'link'} link={''}>
+								<BookOpenTextIcon weight={'bold'} fontSize={'24'} />
+								<span>When Sorry Isn't Enough</span>
+								<ArrowUpRightIcon weight={'bold'} fontSize={'24'} />
+							</Button>
+						</div>
+					</div>
+				</article>
+			</section>
 
-                        {/* Floating actions */}
-                        <div className="archetype-footer flex  justify-evenly  ">
-                            <div className='archetype-footer-div flex justify-between'> 
-                                <div className="archetype-body-info-right-navigation flex items-center justify-between ">
-                                    <span className="archetype-body-info-right-navigation-icon active"><PawPrintIcon weight={'regular'} size={32} /></span>
-                                    <span className="archetype-body-info-right-navigation-icon"><CameraIcon weight={'regular'} size={32} /></span>
-                                    <span className="archetype-body-info-right-navigation-icon"><SneakerMoveIcon weight={'regular'} size={32} /></span>
-                                    <span className="archetype-body-info-right-navigation-icon"><RainbowCloudIcon weight={'regular'} size={32} /></span>
-                                    <span className="archetype-body-info-right-navigation-icon"><MicrophoneStageIcon weight={'regular'}size={32} /></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            {/* Bottom footer */}
-            <div className="md:col-span-2">
-                <div className="archetype-body-footer flex flex-row items-start justify-center w-full">
-                    <div className="archetype-body-footer-card flex flex-col items-center">
-                        <p className="t-highlights">Currently watchingâ€¦</p>
-                        <Button hierarchy={'tertiary'} type={'link'} link={''}>
-                            <FilmSlateIcon weight={'bold'} fontSize={'24'} />
-                            <span>Stranger Things S05</span>
-                            <ArrowUpRightIcon weight={'bold'} fontSize={'24'} />
-                        </Button>
-                    </div>
-                    <div className="divider vertical"/>
-                    <div className="archetype-body-footer-card flex flex-col items-center">
-                        <p className="t-highlights">Currently readingâ€¦</p>
-                        <Button hierarchy={'tertiary'} type={'link'} link={''}>
-                            <BookOpenTextIcon weight={'bold'} fontSize={'24'} />
-                            <span>When Sorry Isn't Enough</span>
-                            <ArrowUpRightIcon weight={'bold'} fontSize={'24'} />
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </article>
-    </section>
 
             <section className="work">
                 {/* Header */}
@@ -279,7 +265,7 @@ const Home = () => {
                         </div>
 
                         <span className="work-item-link t-copy-small child">
-                            <ArrowRightIcon weight='regular' size={32} />s
+                            <ArrowRightIcon weight='regular' size={32} />
                         </span>
                     </div>
 
