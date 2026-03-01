4e45e97c67af87847f7812326cabe2b8df61700d
Jude Joshua <thejudejoshua@gmail.com>
2026-03-01 07:47:37 -0500

    took out cursor and adjusted color on photos and

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './assets/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <Cursor />
+        {/* <Cursor /> */}
         <App />
     </BrowserRouter>,
 )
