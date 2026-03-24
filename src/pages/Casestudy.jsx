1487275adf4317cd17f47cc4cfccfe3495291327
Ping1234-tech <smithcomfort251@gmail.com>
2026-03-24 17:50:47 +0100

    updated version

--- src/pages/Casestudy.jsx
+++ src/pages/Casestudy.jsx
@@ -1,26 +1,27 @@
-import { ArrowLeftIcon } from "@phosphor-icons/react"
+import { ArrowLeftIcon, X } from "@phosphor-icons/react"
 import caseStudyImage from '../assets/images/case-study.png';
 import markerImage from '../assets/images/marker-2.svg';
 import pageIcon from '../assets/images/page-icon.png';
 import imageOkafor from '../assets/images/Okafor.png';
 import imageSarah from '../assets/images/Sarah.png';
-import crossIcon from '../assets/images/wrong.png';
+import crossImage from '../assets/images/wrong.png';
+import diagramImage from '../assets/images/diagram.png'
 
 
 export default function CaseStudy() {
 	return (
     <>
-      <section className="hero flex flex-col items-center justify-center">
-			<div className=" w-full max-w-3xl">
+      <section className="casestudy flex flex-col items-center justify-center">
+			<div className="casestudy w-full max-w-3xl">
 
-				<div className="flex items-center text-emerald-600 font-bold !m-[40px] justify-center">
+				<div className="casestudy-archives flex items-center justify-center">
 					<span><ArrowLeftIcon weight="bold" /></span>
 					<a href="/archives">Back to Archives</a>
 				</div>
 
 				<div>
-					<h2 className="text-3xl md:text-5xl font-bold text-black-500 text-center max-w-3xl mx-auto leading-tight">
-						<span className="text-emerald-600 flex flex-col items-center inline-block !mr-3">
+					<h2 className="casestudy-header h2 md:h1 text-center max-w-3xl">
+						<span className="casestudy-header-span flex flex-col items-center inline-block">
 							How might we
 							<img src={markerImage} alt="Marker" className="w-80 h-6" />
 						</span>
@@ -28,34 +29,34 @@ export default function CaseStudy() {
 					</h2>
 				</div>
 
-				<div className="!mt-16 bg-slate-100 rounded-sm items-center">
-					<div className="flex flex-col gap-2  items-center mx-auto !py-2">
-						<div className="w-[98%]">
+				<div className="casestudy-box items-center">
+					<div className="casestudy-box-inner flex flex-col items-center ">
+						<div className="casestudy-box-inner">
 							<img src={caseStudyImage} alt="Case Study" className="  rounded" />
 						</div>
 
-						<div className="grid grid-cols-1 md:grid-cols-3 gap-8 !px-12 !py-8 rounded-sm h-full bg-white w-[98%] items-center justify-center">
-							<div className="border-l md:border-l-0 md:border-r border-slate-200 pl-6 md:pl-0">
-								<p className="text-xs font-bold uppercase tracking-widest text-slate-800 mb-2 flex items-center">
+						<div className="casestudy-grid casestudy-box-inner grid grid-cols-3 items-start justify-between">
+							<div className="casestudy-border-none">
+								<p className="casestudy-par t-highlights flex items-center justify-center">
 									Role 💼
 								</p>
-								<p className="text-slate-600 text-sm">Product Designer</p>
+								<p className="casestudy-par-1">Product Designer</p>
 							</div>
 
-							<div className="border-l md:border-l-0 md:border-r border-slate-200 pl-6 md:pl-0">
-								<p className="text-xs font-bold uppercase tracking-widest text-slate-800 mb-2 flex items-center">
+							<div className="casestudy-border">
+								<p className="casestudy-par t-highlights flex items-center justify-center">
 									Scope 🎯
 								</p>
-								<p className="text-slate-600 text-sm leading-relaxed">
+								<p className="casestudy-par-1">
 									UX strategy, UI redesign,<br />interaction design
 								</p>
 							</div>
 
-							<div className=" border-slate-200 pl-6">
-								<p className="text-xs font-bold uppercase tracking-widest text-slate-800 mb-2 flex items-center">
+							<div className="casestudy-border">
+								<p className="casestudy-par t-highlights flex items-center justify-center">
 									Focus 🔍
 								</p>
-								<p className="text-slate-600 text-sm leading-relaxed">
+								<p className="casestudy-par-1">
 									Reduce friction in product discovery and checkout.
 								</p>
 							</div>
@@ -71,93 +72,59 @@ export default function CaseStudy() {
     {/* Context section */}
   <section className="context flex flex-col items-center justify-center">
 	<div className="w-full max-w-3xl">
-    <div className="context flex items-center gap-2">
-      <h6 className="h6">CONTEXT</h6>
-      <img src={pageIcon} alt="Page Icon" className="w-6 h-6" />
-    </div>
+	<div className="context flex justify-center items-center">
+	  <h6 className="h6">CONTEXT</h6>
+	  <img src={pageIcon} alt="Page Icon" className="w-6 h-6" />
+	</div>
 
-    <div className="context-header max-w-3xl text-center leading-tight">
-      <h3 className="h4 font-bold">Something's Wrong.Our users are not buying from the website.</h3>
-    </div>
+	<div className="context-header max-w-3xl text-center leading-tight">
+	  <h3 className="h4 font-bold">Something's Wrong.Our users are not buying from the website.</h3>
+	</div>
 
-    <div className="context-box max-w-3xl items-center">
-      <div className="context-box-extra items-center-small">
+	<div className="context-box max-w-3xl items-center">
+	  <div className="context-box-extra items-center-small">
 		<div className="context-box-extra-par t-copy leading-tight tracking-tight"> 
-		    <p> FirstMedtrade is an online marketplace connecting healthcare providers and individuals to verified medical
+			<p> FirstMedtrade is an online marketplace connecting healthcare providers and individuals to verified medical
 			 supplies. The platform offered a wide catalogue of products, but the buying experience did not match the 
 			 urgency and precision required in healthcare procurement.<br/></p>
 
-             <p className="context-box-extra-par-1">I was responsible for rethinking the product experience from discovery to checkout,<b> with the goal of understanding 
-             why users were not buying from the platform.</b>
+			 <p className="context-box-extra-par-1">I was responsible for rethinking the product experience from discovery to checkout,<b> with the goal of understanding 
+			 why users were not buying from the platform.</b>
 			 </p>
 		 </div>
-      </div>
-    </div>
-
-	<div className="context-header max-w-3xl text-center leading-tight">
-      <h3 className="h4 font-bold">The users pattern after monitoring the website for a month was something like this:</h3>
-    </div>
-
-	<div className="flow">
-        <div className="flow-row flex items-center justify-center">
-			<div className="flow-container flow-container-start text-center ">
-				<h6 className="h6">Start</h6>
-				<p className="">User visits firstmedtrade.com</p>
-			</div>
-
-			<div className="flow-arrow"></div>
-
-			<div className="flow-container flow-container-search text-center">
-               <h6 className="h6">Search</h6>
-			   <p className="tracking-wide">User searches for products using search bar.</p>
-			</div>
-
-			<div className="flow-arrow"></div>
-
-			<div className="flow-container flow-container-screenshot text-center">
-				<h6 className="h6">Screenshot</h6>
-				<p className="">User finds product and takes a screenshot.</p>
-			</div>
-			</div>
-
-			{/*bottom row*/}
-			<div className="flow flow-bottom flex justify-between">
-				<div className="flow-container flow-container-result text-center">
-					<h6 className="h6">Result</h6>
-					<p className="">Support is overwhelmed and unable to keep up with the messages from multiple users.</p>
-				</div>
-
-				<div className="vertical-line"></div>
-
-				<div className="flow-container flow-container-drop text-center">
-					<h6 className="h6">Dropoff</h6>
-					<p>User takes screenshot to WhatsApp to check product availability and details.</p>
-				</div>
-			</div>
-
-			
+	  </div>
 	</div>
 
-  </div>
+         {/*Diagram*/}
+	<div className="context-header max-w-3xl text-center leading-tight">
+	  <h3 className="h4 font-bold">The users pattern after monitoring the website for a month was something like this:</h3>
+	</div>
+
+	<div className="context-diagram">
+        <img src={diagramImage} alt="image" />
+	</div>
+
+	</div>
   </section>
 
+
   {/*Tension section*/}
-  <section className="flex flex-col justify-center items-center">
+  <section className="tension flex flex-col justify-center items-center">
 	<div className="w-full max-w-3xl">
-		<div className="context flex items-center gap-2">
+		<div className="tension flex justify-center items-center ">
       <h6 className="h6">TENSION</h6>
-      <img src={crossIcon} alt="Page Icon" className="w-6 h-6" />
+        <img src={crossImage} alt="Wrong"  className="w-5 h-5"/>
     </div>
 
-	<div className="context-header max-w-3xl text-center leading-tight">
+	<div className="tension-header max-w-3xl text-center leading-tight">
       <h3 className="h4 font-bold">Buyers operate under pressure.They do not browse casually.
 		They browse with intent.
 	  </h3>
     </div>
 
-	<div>
-		<img src={imageOkafor} alt="" />
-		<img src={imageSarah} alt="" />
+	<div className="tension-image flex">
+		<img src={imageOkafor} alt="okafor" />
+		<img src={imageSarah} alt="sarah" />
 	</div>
 
 
