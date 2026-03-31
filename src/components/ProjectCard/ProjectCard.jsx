ef7da13bd952f4d393d62432c400567fb7863cb0
Ping1234-tech <smithcomfort251@gmail.com>
2026-04-01 00:10:29 +0100

    updated project page

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,50 +1,56 @@
-import './projectCard.scss';
+<section className="project-card">
+    <div className="project-card">
+        <div className="project-card-top-shape"> </div>
+        <div className="project-card-box-1">
+            <p className="t-copy tracking-wide">UI/UX</p>
+        </div>
 
-import { ArrowRightIcon } from '@phosphor-icons/react';
+        <h6 className="project-card-title h6">Redesigning a unified B2B and B2C web app for healthcare
+            providers and seekers.
+        </h6>
 
-import SampleImage from '../../assets/images/fmta-screen.webp'
+        <div className="project-card-bottom-shape"> 
+         <p className="t-copy">How might we reduce cognitive load and make ordering medical
+             supplies feel immediate, controlled and trustworthy?</p>
+        </div>
 
-import ImageComponent from "../ImageComponent/ImageComponent.jsx";
-import Button from "../Button/Button.jsx";
+    </div>
+
+    {/*second card*/}
+    <div className="project-card">
+        <div className="project-card-top-shape"> </div>
+        <div className="project-card-box-1">
+            <p className="t-copy tracking-wide">UI/UX</p>
+        </div>
+
+        <h6 className="project-card-title h6">Redesigning a unified B2B and B2C web app for healthcare
+            providers and seekers.
+        </h6>
+
+        <div className="project-card-bottom-shape"> 
+         <p className="t-copy">How might we reduce cognitive load and make ordering medical
+             supplies feel immediate, controlled and trustworthy?</p>
+        </div>
+
+    </div>
+
+    {/*third card*/
+    <div className="project-card">
+        <div className="project-card-top-shape"> </div>
+        <div className="project-card-box-1">
+            <p className="t-copy tracking-wide">UI/UX</p>
+        </div>
+
+        <h6 className="project-card-title h6">Redesigning a unified B2B and B2C web app for healthcare
+            providers and seekers.
+        </h6>
+
+        <div className="project-card-bottom-shape"> 
+         <p className="t-copy">How might we reduce cognitive load and make ordering medical
+             supplies feel immediate, controlled and trustworthy?</p>
+        </div>
+
+    </div>}
+</section>
 
 
-const ProjectCard = ( { variant } ) => {
-
-    switch (variant) {
-        case 'long':
-            return (
-                <div className="project long grid grid-cols-8 justify-start items-center">
-                    <div className="project-image col-span-5">
-                        <ImageComponent src={SampleImage} className={''} alt={'sample project image'} />
-                    </div>
-                    <div className="project-details col-span-3 flex flex-col justify-center">
-                        <div className="project-details-labels flex flex-row">
-                            <span className="t-caption">UI/UX</span>
-                        </div>
-
-                        <div className="project-details-text flex flex-col items-start justify-start">
-                            <div className="project-details-text-holder flex flex-col items-start justify-start">
-                                <h6 className="h6">
-                                    Redesigning a unified B2B and B2C web app for healthcare providers and seekers.
-                                </h6>
-                                <p className="t-copy-small">
-                                    How might we reduce cognitive load and make ordering medical supplies feel immediate, controlled, and trustworthy?
-                                </p>
-                            </div>
-                            <Button hierarchy={'tertiary'} type={'link'} link={'/projects/firstmedtrade'}>
-                                <span>View case study</span>
-                                <ArrowRightIcon weight={'bold'} fontSize={'24'} />
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            );
-
-        default:
-            return null;
-    }
-
-
-}
-
-export default ProjectCard;
