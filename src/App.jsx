82b1489739e94ea37fd582a8da6ee78c2b1f62fe
Jude Joshua <thejudejoshua@gmail.com>
2026-05-14 11:57:57 -0700

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
                 <Route path="/" element={<Layout><Home /></Layout>} />
                 <Route path="/about" element={<Layout navbarVariant="notfound" footerVariant="notfound"><Maintenance/></Layout>} />
                 <Route path="/projects" element={<Layout><Projects /></Layout>} />
-                <Route path="/projects/:id" element={<Layout><Maintenance /></Layout>} />
+                <Route path="/projects/:id" element={<Layout navbarVariant="notfound" footerVariant="notfound"><Maintenance /></Layout>} />
                 <Route path="*" element={<Layout navbarVariant="notfound" footerVariant="notfound"><NotFound/></Layout>} />
                 
                 <Route path="/no-javascript" element={<Layout navbarVariant="notfound" footerVariant="notfound"><Maintenance /></Layout>} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,16 +18,18 @@ export default function Navbar({ variant = "default" }) {
 	return (
 		<nav className={navClass + " fade-section-down fade-section--delay-2 flex flex-row justify-between align-center"}>
 			<div className="navbar-logo fade-section-down fade-section--delay-2">
-				<Logo />
+				<Url href={'/'}>
+					<Logo />
+				</Url>
 			</div>
 			<div className="navbar-links fade-section-down fade-section--delay-3 flex flex-row items-center justify-center">
-				<NavbarLink navLinks={Links}/>
+				<NavbarLink navLinks={Links} />
 				<div className="divider navbar-links-navigation-divider"></div>
 				<Url href='mailto:work@judejoshua.com' className="navbar-links-navigation-mailer"
-				     isNavigationLink={true}>
-                    <span>
-                        work@judejoshua.com
-                    </span>
+					isNavigationLink={true}>
+					<span>
+						work@judejoshua.com
+					</span>
 				</Url>
 			</div>
 		</nav>
